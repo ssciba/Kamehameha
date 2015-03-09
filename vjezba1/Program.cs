f55cd0040e152dd66074658720b50e458b8c334a
ssciba <mcibar00@fesb.hr>
2015-03-09 09:18:56 +0100

    Update Program.cs
    greska

--- vjezba1/Program.cs
+++ vjezba1/Program.cs
@@ -10,7 +10,7 @@ namespace vjezba1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Sućidar");
+            Console.WriteLine("Sućidar
         }
     }
 }
